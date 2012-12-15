88e6d40f938bc3d26a654784fd063c4144de2bd8
igor <igor.veremchuk@gmail.com>
2012-12-15 23:33:16 +0200

    added jenkins

--- txtr/settings.py
+++ txtr/settings.py
@@ -143,7 +143,14 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'service',
+    'django_jenkins',
 )
+
+JENKINS_TASKS = ('django_jenkins.tasks.run_pylint',
+                 'django_jenkins.tasks.run_pep8',
+                 'django_jenkins.tasks.run_pyflakes',
+                 'django_jenkins.tasks.with_coverage',
+                 'django_jenkins.tasks.django_tests',)
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
